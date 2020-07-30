7d577972f0cec06f0bba1d1da1eb862a11ffc6c0
Zane Chandy <zanechandy@zanes-mbp.attlocal.net>
2020-07-29 23:24:29 -0400

    Add scripts folder

---
+++ scripts/dev_to_test.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+#
+# The procedure goes in this script as follows:
+# 
+# git pull origin master
+# rm dev_to_test.sh
+# cd ~/staging
+# ./test_to_stage.sh
+#
+
+FILE=~/script_exec_log.txt
+
+if [ -f "$FILE" ]; then
+        date >> $FILE        
+        echo -e "Note: file already exists. Top logo will not be created.\n" >> $FILE
+else
+        cd ~
+        touch script_exec_log.txt
+        echo -e "***********************************************\n" >> $FILE
+        echo -e "     ENVIRONMENT SCRIPT EXECUTION LOG FILE     \n" >> $FILE
+        echo -e "***********************************************\n\n" >> $FILE
+        echo -e "<weekday month day UT timezone>\n" >> $FILE 
+        echo -e "<description>\n\n" >> $FILE    
+fi
+
+wait 
+
+date >> $FILE 
+echo -e ": Waited successfully\n" >> $FILE 
+
+date >> $FILE
+echo -e ": Beginning dev_to_test.sh\n" >> $FILE
+
+date >> $FILE
+echo -e ": (command: git pull origin master) " >> $FILE
+git pull origin master >> $FILE
+echo -e "\n" >> $FILE
+
+wait
+
+date >> $FILE
+echo -e ": Waited successfully\n" >> $FILE
+
+date >> $FILE
+echo -e ": dev_to_test.sh executed successfully\n" >> $FILE
+
+cd ~/staging
+
+date >> $FILE
+echo -e ": Changed directory to ~/staging. Waiting and returning exit status 0.\n" >> $FILE
+
+./test_to_stage.sh
+
+wait
+
+exit 0
---
+++ scripts/test_to_stage.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+#
+#
+# The procedure goes in this script as follows:
+# 
+# \cp -r ~/testing/* ~/staging
+# rm dev_to_test.sh
+# sendmail
+# delete email.txt
+#
+
+FILE=~/script_exec_log.txt
+EMAIL=~/staging/email.txt
+
+date >> $FILE
+echo -e ": Starting test_to_stage.sh\n" >> $FILE
+
+date >> $FILE
+echo -e ": (command: \cp ~/testing/* ~/staging #waiting) " >> $FILE
+\cp -r ~/testing/* ~/staging >> $FILE 
+
+wait
+
+echo -e "\n" >> $FILE
+
+date >> $FILE
+echo -e ": Waited successfully\n" >> $FILE
+
+date >> $FILE
+echo -e ": Files successfully moved from testing to staging\n" >> $FILE
+
+date >> $FILE
+echo ": (command: ls) " >> $FILE
+ls >> $FILE
+echo -e "\n" >> $FILE
+
+wait
+
+date >> $FILE
+echo -e ": Waited successfully\n" >> $FILE
+
+if [ -f "$FILE" ]; then
+        date >> $FILE        
+        echo -e "Note: file already exists. Top logo will not be created.\n" >> $FILE
+else
+        cd ~
+        touch script_exec_log.txt
+        echo -e "***********************************************\n" >> $FILE
+        echo -e "     ENVIRONMENT SCRIPT EXECUTION LOG FILE     \n" >> $FILE
+        echo -e "***********************************************\n\n" >> $FILE
+        echo -e "<weekday month day UT timezone>\n" >> $FILE 
+        echo -e "<description>\n\n" >> $FILE    
+fi
+
+wait 
+
+date >> $FILE
+echo -e ": Waited successfully\n" >> $FILE
+
+date >> $FILE
+echo ": (command: rm dev_to_test.sh) " >> $FILE 
+rm dev_to_test.sh >> $FILE
+echo -e "\n" >> $FILE
+
+date >> $FILE
+echo -e ": Cloned dev_to_test.sh removed successfully\n" >> $FILE 
+
+echo -e "Subject: Transfer to Staging successful\n" >> $EMAIL
+echo -e "Files have been successfully transferred from Testing to Staging and are ready to be deployed in Production.\n" >> $EMAIL
+git log >> $EMAIL
+echo -e "\n" >> $EMAIL
+
+wait
+
+date >> $FILE
+echo -e ": Waited successfully\n" >> $FILE
+
+date >> $FILE
+echo -e ": Created email.txt\n" >> $FILE
+
+wait
+
+date >> $FILE
+echo -e ": Waited successfully\n" >> $FILE
+
+date >> $FILE
+echo ": (command: /usr/sbin/sendmail zanechandy < email.txt) " >> $FILE
+/usr/sbin/sendmail zanechandygmail.com < email.txt >> $FILE
+echo -e "\n" >> $FILE
+
+wait
+
+date >> $FILE
+echo -e ": Waited successfully\n" >> $FILE
+
+date >> $FILE
+echo -e ": email successfully sent\n" >> $FILE
+
+date >> $FILE
+echo ": (command: rm email.txt) " >> $FILE
+rm email.txt >> $FILE
+echo -e "\n" >> $FILE
+
+wait
+
+date >> $FILE
+echo -e ": Waited successfully\n" >> $FILE
+
+date >> $FILE
+echo -e ": email.txt successfully deleted\n" >> $FILE
+
+date >> $FILE
+echo -e ": test_to_stage.sh executed successfully. Waiting and returning exit status 0.\n" >> $FILE
+
+wait
+
+exit 0
