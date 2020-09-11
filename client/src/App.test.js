6c341b1d9ee212858457fdbf621841a1532ec28e
Zane Chandy <zanechandy@Zanes-MacBook-Pro.local>
2020-09-11 13:42:34 -0400

    Add mobile css and privacy policy page

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { resetToggleDisplay } from './App';
+import { toggleDisplayNav } from './components/Header.js';
+
+/**
+ *  Testing library for front-end. Includes boilerplate code originally by using "create-react-app". 
+ *  To run this file, use "npm test". - Zane
+ */
 
 const originalConsoleError = console.error;
 
+// App render testing
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
 
-// Proptype testing
+// Proptype testing for all passed props in components.
 console.error = message => {
   if (/(Failed prop type)/.test(message)) {
     throw new Error(message);
@@ -17,3 +24,13 @@ console.error = message => {
 
   originalConsoleError(message);
 };
+
+// toggleDisplayNav() testing; src: client/src/components/Header.js
+it("doesn't output anything", () => {
+  expect(toggleDisplayNav).toBe(undefined);
+});
+
+// resetToggleDisplay() testing; src: client/src/App.js
+it("doesn't output anything", () => {
+  expect(resetToggleDisplay).toBe(undefined);
+});
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,28 +3,92 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export default class Header extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            toggleDisplay: false
+        }
+        this.toggleDisplayNav = this.toggleDisplayNav.bind(this);
+    }
+
+    /**
+     * 
+     * toggleDisplayNav() funcion - An event handler for hamburger-icon to toggle display the mobile nav.
+     * Changes being made to CSS during toggleDisplayNav:
+     * 
+     * nav > div > ul {
+     *  display: block;
+     *  > li {
+     *      display: block;
+     *      padding-bottom: 25px;
+     * }
+     * 
+     */
+    toggleDisplayNav() {
+        if (this.state.toggleDisplay) {
+            const hamburgerIcon = document.getElementsByClassName("hamburger-icon")[0];
+            const navDiv = document.getElementsByTagName("div")[1];
+
+            // Change hamburgerIcon's display to block; change navDiv margin and transition settings.
+            hamburgerIcon.style.display = "block";
+            navDiv.style.marginTop = "-485.72px";
+            navDiv.style.transition = "margin-top 2s";
+
+            // Set global boolean variable of toggle state to false.
+            this.setState({ toggleDisplay: false });   
+        } else {
+            const hamburgerIcon = document.getElementsByClassName("hamburger-icon")[0];
+            const navDiv = document.getElementsByTagName("div")[1];
+            const headerLinks = document.getElementsByClassName("header-links")[0];
+
+            // Change hamburgerIcon's display to none; change navDiv and headerLinks display to block.
+            hamburgerIcon.style.display = "none";
+            headerLinks.style.display = "block";
+
+            // Change navDiv margin and transition settings.
+            navDiv.style.marginTop  = "0px";
+            navDiv.style.transition = "margin-top 2s";
+            
+            // Iterate through all <li> tags of class "header-links" and set display to block and add padding-bottom of 25px.
+            for (let childNode = 0; childNode < headerLinks.childNodes.length; childNode += 2) {
+                headerLinks.childNodes[childNode].style.display = "block";
+                headerLinks.childNodes[childNode].style.paddingBottom = "25px";
+            }
+            
+            // Set global boolean variable of toggle state to true.
+            this.setState({ toggleDisplay: true });
+        }
+    }
+
     render() {
-        return(        
-            <header>
+        return(
+            <header> 
                 <nav>
-                    <Link to="/"><img className="logo" srcSet={this.props.logo} alt="New Haven Native American Church logo" width="221px" height="210px" /></Link>
-                    <ul className="header-links">
-                        <li><Link className="nav-footer-link" to="/">Home</Link></li> 
-                        <li className="vertical-bar">|</li>
-                        <li><Link className="nav-footer-link" to="#">News</Link></li>
-                        <li className="vertical-bar">|</li>
-                        <li><Link className="nav-footer-link" to="#">NHNAC University</Link></li>
-                        <li className="vertical-bar">|</li>
-                        <li><Link className="nav-footer-link" to="#">About</Link></li>
-                        <li className="vertical-bar">|</li>
-                        <li><Link className="nav-footer-link" to="#">Donate</Link></li>
-                    </ul>
+                    <img className="hamburger-icon" onClick={this.toggleDisplayNav} srcSet={this.props.hbIcon} alt="Hamburger menu icon" /> 
+                    <div>
+                        <img className="cross" onClick={this.toggleDisplayNav} srcSet={this.props.cross} alt="Exit icon" />
+                        <Link to="/" onClick={this.resetToggleDisplay}><img className="logo" srcSet={this.props.logo} alt="New Haven Native American Church logo" width="221px" height="210px" /></Link>
+                        <ul className="header-links">
+                            <li><Link className="nav-footer-link" to="/">Home</Link></li> 
+                            <li className="vertical-bar">|</li>
+                            <li><Link className="nav-footer-link" to="#">News</Link></li>
+                            <li className="vertical-bar">|</li>
+                            <li><Link className="nav-footer-link" to="#">NHNAC University</Link></li>
+                            <li className="vertical-bar">|</li>
+                            <li><Link className="nav-footer-link" to="#">About</Link></li>
+                            <li className="vertical-bar">|</li>
+                            <li><Link className="nav-footer-link" to="#">Donate</Link></li>
+                        </ul>
+                    </div>
                 </nav>
             </header>);
     }
 }
 
-// PropTypes
+// PropTypes for jest testing in App.test.js
 Header.propTypes = {
-    logo: PropTypes.string.isRequired
+    logo: PropTypes.string.isRequired,
+    hbIcon: PropTypes.string.isRequired,
+    cross: PropTypes.string.isRequired
 }
